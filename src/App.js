1247219a5c1c508eeb182755ae3c4f164673d246
AllA <allagorik2901@gmail.com>
2017-10-30 22:13:29 -0400

    done with functionality

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react'
+import './App.css'
+import Clock from './Clock'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      deadline: 'December 25, 2017',
+      newDeadLine: ''
+    }
+  }
+
+  changeDeadline = (event) => {
+    this.setState({
+      deadline: this.state.newDeadLine
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className='title'>Countdown to {this.state.deadline}</div>
+            <Clock deadline={this.state.deadline} />
+          <div>
+            <input placeholder='new date' onChange={event => this.setState({newDeadLine: event.target.value})}/>
+            <button onClick={this.changeDeadline}>Submit</button>
+          </div>
+      </div>
+      )
+  }
+}
+
+export default App
